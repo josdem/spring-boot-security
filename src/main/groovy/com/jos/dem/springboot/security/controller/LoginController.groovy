ca8af5a1952158de9605d8349f819b1bfa7804ca
josdem <joseluis.delacruz@gmail.com>
2018-03-14 19:40:34 -0600

    Adding login controller

---
+++ src/main/groovy/com/jos/dem/springboot/security/controller/LoginController.groovy
@@ -0,0 +1,34 @@
+package com.jos.dem.springboot.security.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Controller
+
+import com.jos.dem.vetlog.service.LocaleService
+import com.jos.dem.vetlog.service.VetlogService
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+@Controller
+class LoginController {
+
+  @Autowired
+  LocaleService localeService
+
+  Logger log = LoggerFactory.getLogger(this.class)
+
+  @RequestMapping("/login")
+  ModelAndView login(@RequestParam Optional<String> error){
+    log.info "Calling login"
+    ModelAndView modelAndView = new ModelAndView('login/login')
+    if(error.isPresent()){
+      modelAndView.addObject('error', localeService.getMessage('login.error'))
+    }
+    modelAndView
+  }
+
+}
