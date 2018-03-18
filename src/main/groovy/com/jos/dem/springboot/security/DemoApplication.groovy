3438f5d905ab90410da421955ddee6052727b783
josdem <joseluis.delacruz@gmail.com>
2018-03-18 16:19:59 -0600

    Getting this work with spring boot 1.5.5

--- src/main/groovy/com/jos/dem/springboot/security/DemoApplication.groovy
+++ src/main/groovy/com/jos/dem/springboot/security/DemoApplication.groovy
@@ -1,4 +1,4 @@
-package com.example.springbootsecurity
+package com.jos.dem.springboot.security
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
