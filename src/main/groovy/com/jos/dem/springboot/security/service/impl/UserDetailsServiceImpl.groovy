a81b4a694f5ee9ebe16030c6cea0af07036df7dc
josdem <joseluis.delacruz@gmail.com>
2018-03-19 18:47:04 -0600

    Changing from service to repository

--- src/main/groovy/com/jos/dem/springboot/security/service/impl/UserDetailsServiceImpl.groovy
+++ src/main/groovy/com/jos/dem/springboot/security/service/impl/UserDetailsServiceImpl.groovy
@@ -8,17 +8,17 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 
 import  com.jos.dem.springboot.security.model.User
-import  com.jos.dem.springboot.security.service.UserService
+import  com.jos.dem.springboot.security.repository.UserRepository
 
 @Service
 class UserDetailsServiceImpl implements UserDetailsService {
 
   @Autowired
-  UserService userService
+  UserRepository userRepository
 
   @Override
   org.springframework.security.core.userdetails.User loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userService.getUserByUsername(username)
+    User user = userRepostory.findByUsername(username)
     Set<GrantedAuthority> grantedAuthorities = [] as Set
     grantedAuthorities << new SimpleGrantedAuthority(user.role.toString())
     new org.springframework.security.core.userdetails.User(user.username, user.password, grantedAuthorities)
